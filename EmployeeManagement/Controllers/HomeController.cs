d2df0fc59c40c00f38c174688eb92bbe3ea1ac4c
sharat <sharattbhat@gmail.com>
2019-08-27 13:18:19 +1000

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController: Controller
+    {
+        public JsonResult Index() {
+            return Json(new { id = 1, name = "John" });
+        }
+    }
+}
