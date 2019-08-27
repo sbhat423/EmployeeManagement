d4c8779be9b69d2b9d461539c9bbb32903c9a3a1
sharat <sharattbhat@gmail.com>
2019-08-27 15:07:26 +1000

    Add Employee Model using Dependency Injection

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
 {
     public class HomeController: Controller
     {
-        public JsonResult Index() {
-            return Json(new { id = 1, name = "John" });
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index() {
+            return _employeeRepository.GetEmployee(1).Name;
         }
     }
 }
