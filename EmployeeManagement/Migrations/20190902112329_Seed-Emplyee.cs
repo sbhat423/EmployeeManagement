4d5070992340adcc659f4144002cf0b389731a65
sharat <sharattbhat@gmail.com>
2019-09-02 21:26:56 +1000

    Create migration

---
+++ EmployeeManagement/Migrations/20190902112329_Seed-Emplyee.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class SeedEmplyee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 3, "mark@dumbcompany.com", "Mark" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "joe@dumbcompany.com", "Joe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mark",
+                    Email = "mark@dumbcompany.com",
+                    Department = Dept.IT
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Joe",
+                    Email = "joe@dumbcompany.com",
+                    Department = Dept.Sales
+                }
+            );
+        }
+    }
+}
